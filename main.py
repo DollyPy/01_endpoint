40b8d90cfec7d145e3c4997211b2707d16026868
DollyPy <ddolapo1@gmail.com>
2023-09-07 17:15:09 +0100

    new commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+import time
+app = Flask(__name__)
+
+@app.route('/api', methods = ['GET'])
+def index():
+    slack = request.args.get("slack_name")
+    stack = request.args.get("track")
+    res =  {"slack_name":slack,
+            "current_day":datetime.now().strftime('%A'),
+            "utc_time":datetime.utcnow().isoformat(),
+            "track":stack,
+            "github_file_url":"unknown",
+            "github_repo_url":"unknown",
+            "status_code":200
+            }
+    return jsonify(res)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8888', debug=True)
