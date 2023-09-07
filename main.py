917a45fd1f9b901dd2a20891245803e2a8e5c160
DollyPy <ddolapo1@gmail.com>
2023-09-07 17:57:59 +0100

    Updated timezone settings

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
-import time
+
 app = Flask(__name__)
 
 @app.route('/api', methods = ['GET'])
 def index():
     slack = request.args.get("slack_name")
     stack = request.args.get("track")
+    utc = datetime.utcnow().isoformat().split(".")[0]+"Z"
     res =  {"slack_name":slack,
             "current_day":datetime.now().strftime('%A'),
-            "utc_time":datetime.utcnow().isoformat(),
+            "utc_time": utc,
             "track":stack,
             "github_file_url":"https://github.com/DollyPy/01_endpoint/blob/main/main.py",
             "github_repo_url":"https://github.com/DollyPy/01_endpoint",
